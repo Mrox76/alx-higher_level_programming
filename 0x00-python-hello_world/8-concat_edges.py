3b1c446c7f834e45e6a5cf401635e1603b016fdf
Mrox76 <valdimiroaboo@yahoo.com>
2022-12-07 06:23:51 -0800

    correct the output string

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[38:-54]} {str[107:-18]} {str[:6]}")
+print(f"{str[38:-63]} {str[107:-18]} {str[:6]}")
